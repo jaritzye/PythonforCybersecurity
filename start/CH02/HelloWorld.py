91ab37e6f237b8358cf7837c31dda7b9e0303a98
jaritzye <esquivelhernandez.jaritzy@tudent.greenriver.edu>
2023-10-07 15:27:46 -0700

    Started HelloWrld and HWrld2

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created 
+# Created by Jaritzy on Oct 7
+print ("Hello World")
--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
-# Created 
+# Created by Jaritzy on Oct 8
 
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+#Ask user for the(ir name
+user_name = input("What is your name? ")
+
+#Print Hello and their name
+print ("Hello {0}".format(user_name))
