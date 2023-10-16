a8bd5cf020aff841bf97e51362eab3d501da702b
jaritzye <esquivelhernandez.jaritzy@student.greenriver.edu>
2023-10-15 19:44:23 -0700

    changed cmd to input

--- end/CH03/Conditionals.py
+++ end/CH03/Conditionals.py
@@ -1,27 +1,15 @@
 
-\#!/usr/bin/env python3
+#!/usr/bin/env python3
 # By Jaritzy Esquivel
 # date: 10/15/23
 
-# Suggestion - 
-#   add in <, ==, > one at a time
-#   make each of them if statements initially
-#   Change x and y values to test the various paths
-#   eventually simplfy with if, elif, else
-
-#Import necessary Python Modules
-
-import platform
-import os
-
 
 #Ask if user is having a good day
-def ask_question():
-    answer: input("Is today a good day? (y/n)  ")
+user_input= input("Is_today_a_good_day? (y/n) ):
 
     # First condition test, answer is 'y'
     if answer == 'y':
         print("Yes it is")
-    # Another conditional test, x is equal to y
+    
     
 
