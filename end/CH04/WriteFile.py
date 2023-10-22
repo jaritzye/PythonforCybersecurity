b69ceea8756c7bcaba9e35b264bf4cca10bb6232
jaritzye <esquivelhernandez.jaritzy@student.greenriver.edu>
2023-10-22 15:30:39 -0700

    Read File HW

--- end/CH04/WriteFile.py
+++ end/CH04/WriteFile.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
-# Sample script that writes to a file
-# By Ed Goad
-# 2/27/2021
+# HackMe.Txt HW
+# By Jaritzy Esquivel
+# 10/22/23
 
-# Open file for writing
-test_file = open("testfile.txt", "w")
+#questions to ask user
+
+questions = {}
+
+questions['Name'] = input("What is your name? ")
+questions['Color'] = input("What is your favorite color? ")
+questions['Pet'] = input("What is your first pet's name? ")
+questions['Mom'] = input("What is your mother's maiden name? ")
+questions['school'] = input("What elementary school did you attend? ")
+
 
 # Write lines to the file
-test_file.write("Hello World\n")
-test_file.write("My name is Ed\n")
-test_file.write("I like rubber ducks\n")
+with open("hackme.txt", "w") as file:
+    for key , value in questions.items():
+        file.write(f"{key}: {value}\n")
 
-# Close the file
-test_file.close()
+#confirmation
+print("Information stored in hackme.txt")
+
+#Read file
+with open("hackme.txt", "r") as file:
+    file_contents =  file.read()
+
+
+print("Here is someone to hack- information")
+
+
+#Print the information from hackme.txt
+print(file_contents)
 
