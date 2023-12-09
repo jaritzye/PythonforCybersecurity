1be71d075bdef8eaa48f34c0288402100107eb6a
jaritzye <esquivelhernandez.jaritzy@tudent.greenriver.edu>
2023-12-09 15:14:51 -0800

    find404.py mod'd

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Jaritzy Esquivel
+
+
+#Prompt for file to analyze
+log_file = input("Which file to analyze?")
+
+#Open file
+f = open(log_file, "r")
+
+#Read file line by line 
+while True:
+    line = f.readline()
+    if not line:
+        break
+    #Check for 404
+    if "404" in line:
+        print(line.strip())
+
+# Close file
+f.close()
