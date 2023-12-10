ef32e201bd566597cdc2e76412bae070e2e3f474
jaritzye <esquivelhernandez.jaritzy@tudent.greenriver.edu>
2023-12-09 16:41:58 -0800

    find client 1

--- start/CH07/FindClients.py
+++ start/CH07/FindClients.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for client addresses
 # Use RegEx to find and report on most frequent users
-# By 
+# By Jaritzy Esquivel
+
+import os
+#Get script dir
+script_dir = os.path.dirname(__file__)
+
+#Import python modules
+import re
+
+#Prompt for file to analyze
+log_file = input("Which file to analyze?")
+
+#Open file and load into memory
+with open(script_dir + "/" + log_file, "r") as f:
+    sample_logs = f.readlines()
+
+#Set up regex pattern and empty dictionary
+client_pattern = r'(^\S+\.[\S+\.]+\S+)\s'
+clientdict = {}
+
+#Find match and store in dictionary
+for line in sample_logs:
+    #Search for pattern, and if found move forward
+    m = re.search(client_pattern, line)
+    if m:
+        client = m.group()
+        #Put access frequency in dictionary
+        if client in clientdict.keys():
+            clientdict[client] += 1
+        else:
+            clientdict[client] = 1
+
+
+#Sort by most frequently accessed
+for w in sorted(clientdict, key=clientdict.get, reverse=False):
+    print(w, clientdict[w])
