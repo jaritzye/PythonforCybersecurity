2ff23691d041fe5a5cfdfaae49a89ecc7953a606
jaritzye <146392125+jaritzye@users.noreply.github.com>
2023-12-09 18:04:01 -0800

    Update HaveIBeenPwned.py

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 # Script that checks passwords agains haveibeenpwned.com API
 # https://haveibeenpwned.com/API/v3#PwnedPasswords
-# By 
+# By Jaritzy Esquivel
+
+print (hello)
