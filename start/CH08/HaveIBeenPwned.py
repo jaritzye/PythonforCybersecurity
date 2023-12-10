266b41f4b065e5f3d5fe86b56ddd6f9d17ca7e1d
jaritzye <esquivelhernandez.jaritzy@tudent.greenriver.edu>
2023-12-09 17:57:55 -0800

    ch 8 hw

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -2,3 +2,6 @@
 # Script that checks passwords agains haveibeenpwned.com API
 # https://haveibeenpwned.com/API/v3#PwnedPasswords
 # By 
+#Step 1: Review documentation
+#Step 2: Try any online examples, demos or tools
+#Step 3:
